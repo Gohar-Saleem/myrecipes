13c39d51aba6d47279b9058fca04e5b6494dcf77
Gohar Saleem <Gohar@EMAIL.com>
2022-07-28 18:23:56 +0500

    first commit

---
+++ app/models/chef.rb
@@ -0,0 +1,8 @@
+class Chef < ApplicationRecord
+    before_save { self.email=email.downcase }
+    validates :chefname, presence: true, length: {maximum: 30}
+    validates :email, presence: true, length: {maximum: 255}, uniqueness: {case_sensitive: false}
+    has_many :recipes, dependent: :destroy
+    has_secure_password
+    validates :password, presence: true, length: {minimum: 5}, allow_nil: true
+end
---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root "pages#home"
+  get 'pages/home', to: 'pages#home'
+
+  resources :recipes
+  get '/signup', to: 'chefs#new'
+  resources :chefs, except: [:new]
+  # Defines the root path route ("/")
+  # root "articles#index"
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+end
